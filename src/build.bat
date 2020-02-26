2284b56a6f43b9ae980b8965c9dfa3016d8d96f1
lovac42 <lovac42@protonmail.com>
2020-02-25 23:44:31 -0500

    version bump v0.2.0

--- src/build.bat
+++ src/build.bat
@@ -3,7 +3,7 @@ set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=spelling_police
 set NAME=Spelling Police
 set PACKID=1410276506
-set VERSION=0.1.0
+set VERSION=0.2.0
 
 
 quick_manifest.exe "%NAME%" "%PACKID%" >%REPO%\manifest.json
